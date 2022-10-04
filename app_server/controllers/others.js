69e2f9a230ae75e9c56415ceb6b1021a02ab84cb
Skyler Espinoza <skyler.espinoza@sckans.edu>
2022-10-04 15:56:32 -0500

    creating home.jade and contact.jade

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,3 +1,28 @@
-module.exports.index = function(req, res){
-    res.render('index', {title: 'FilmCube'});
+const index = function(req, res){
+    res.render('index', { title: 'Express' });
+};
+   
+
+const home = function(req, res){
+    res.render('index', {title: 'Home' });
+};
+
+const info = function(req, res){
+    res.render('index', {title: 'Info' });
+};
+
+const addReview = function(req, res){
+    res.render('index', {title: 'Add Review' });
+};
+
+const contact = function(req, res){
+    res.render('index', {title: 'Contact' });
+};
+
+module.exports = {
+    index,
+    home,
+    info,
+    addReview,
+    contact
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const ctrlPictures = require('../controllers/pictures');
+const ctrlOthers = require('../controllers/others');
 
-var ctrlPictures = require('../controllers/pictures');
-var ctrlOthers = require('../controllers/others');
+router.get('/', ctrlOthers.home);
+router.get('/information', ctrlOthers.info);
+router.get('/review', ctrlOthers.addReview);
+router.get('/contact', ctrlOthers.contact);
 
-router.get('/', ctrlOthers.index, ctrlPictures.index);
+router.get('/index', ctrlOthers.index)//, ctrlPictures.index);
+
+/* Get 'home' page*/
+module.exports.index = function(req, res){ // create an index export method
+  res.render('index', { title: 'Express' }); // coontroller code for homepage
+ };
 module.exports = router;
